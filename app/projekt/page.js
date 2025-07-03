7e148d12fca3e1c146802c0b946ea02a591cd641
klotho17 <moira.s.walter@gmail.com>
2025-07-03 18:38:56 +0200

    try fix

--- app/projekt/page.js
+++ app/projekt/page.js
@@ -4,9 +4,9 @@ import Link from 'next/link';
 export default function ProjectPage() {
   return (
     <div className="project-page">
-      <Link href="/">
+      // Add a link to the main page
       <ProjectIcon width={70} height={40} />
-      </Link>
+      
       <h1>Projekt</h1>
       <p>
         Informationen zum Projekt/Masterarbeit
